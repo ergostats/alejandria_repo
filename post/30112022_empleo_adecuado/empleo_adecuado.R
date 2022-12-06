d81180756fd62f5526f0b4d35d2639a62a8b6613
Andrea Díaz Sánchez <105604988+Anissanchez1971@users.noreply.github.com>
2022-12-06 15:44:30 -0500

    cambios nuevos

--- post/30112022_empleo_adecuado/empleo_adecuado.R
+++ post/30112022_empleo_adecuado/empleo_adecuado.R
@@ -1,19 +1,28 @@
 
 
-# Librerías ---------------------------------------------------------------
+# Declaracion de las Librerías --------------------------------------------
 
 library(haven)
-library(magrittr)
 library(tidyverse)
 library(srvyr)
-
+library(scales)
+library(gt)
+library(gtExtras)
 
 # Lectura de la base de datos ---------------------------------------------
 
-enemdu_persona_2021 <- read_sav("C:/Users/andre/Downloads/1_BDD_ENEMDU_2021_12_SPSS/enemdu_persona_2021_12.sav")
+# 1. Crear un proyecto con la base de datos
+# 2. Guardar la base de datos comprimida en una carpeta data/ dentro del proyecto
+# 3. Con la carpeta data/ simplificamos la ruta:
 
 
-# Diseño muestral ---------------------------------------------------------
+enemdu_persona_2021 <- read_sav("data/enemdu_persona_2021_12.sav")
+
+
+# 4. A la hora de hacer `git add` evitar subir la carpeta data/
+
+
+# Diseño muestral de la base de datos  ------------------------------------
 
 svy <-  enemdu_persona_2021 %>%  
   as_survey_design(ids = upm, 
@@ -22,7 +31,7 @@ svy <-  enemdu_persona_2021 %>%
 
 
 
-# Creación de Variables ---------------------------------------------------
+# Creación de Variables pea y pea adecuado --------------------------------
 
 base1 <- svy %>% 
   mutate(pea1 = if_else(condact == 1 | condact == 8, true = 1, false = 0),
@@ -31,7 +40,7 @@ base1 <- svy %>%
 
 
 
-# Creacion de tablas ------------------------------------------------------
+# Creacion de tablas para las variables pea y pea adecuado ----------------
 
 pea <- base1 %>% 
   group_by(p10a) %>% 
@@ -42,7 +51,7 @@ pea_adecuado <- base1 %>%
   summarise(pea_adecuado1 = survey_total(pea_adecuado1))
 
 
-# Unión de las tablas -----------------------------------------------------
+# Unión de las tablas anteriores en una sola tabla ------------------------
 
 inner_join (pea, pea_adecuado, by = "p10a")  
 
